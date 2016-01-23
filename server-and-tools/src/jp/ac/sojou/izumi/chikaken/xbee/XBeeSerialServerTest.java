7c1b62eae05bc284536a8b563afe7ecd0068c618
Shinobu Izumi <izumi@cis.sojo-u.ac.jp>
2016-01-23 12:57:00 +0900

    update

--- server-and-tools/src/jp/ac/sojou/izumi/chikaken/xbee/XBeeSerialServerTest.java
+++ server-and-tools/src/jp/ac/sojou/izumi/chikaken/xbee/XBeeSerialServerTest.java
@@ -21,96 +21,45 @@ public class XBeeSerialServerTest {
 
 		byte[] buf = new byte[65535];
 
-		for (int i = 0; i < 1000; i++) {
-			int idx = 0;
+		for (int j = 0; j < 1000; j++) {
 
 			byte r = r();
 			byte g = r();
 			byte b = r();
-			r = (byte) 0xff;
-			g = (byte) 0x80;
-			b = (byte) 0xff;
-			{
+
+			int idx = 0;
+			for (int i = 0; i <= 28; i++) {
+
+				if (i % 8 == 0)
+					continue;
+
+				// r = (byte) 0xff;
+				// g = (byte) 0x80;
+				// b = (byte) 0xff;
+
 				byte h = (byte) 0x3f;
 
-				// r = (byte) 0b10111111;
-				// g = (byte) (0b10000001*i);
-				// b = (byte) 0b11111111;
-				byte mask = (byte) 0x7f;
+				byte id = (byte) (((byte) 0x80) | ((byte) i));
+				// System.out.println(Integer.toBinaryString(id));
 
 				buf[idx++] = h;
 				buf[idx++] = r;
 				buf[idx++] = g;
 				buf[idx++] = b;
-				buf[idx++] = mask;
-
-				// bos.write(h);
-				// bos.write(r);
-				// bos.write(g);
-				// bos.write(b);
-				// bos.write(mask);
-
-			}
-			{
-				byte h = (byte) 0x3f;
-				// byte r = r();
-				// byte g = r();
-				// byte b = r();
-				// r = (byte) (0b10000001*i);
-				// g = (byte) 0b11111111;
-				// b = (byte) 0b11111111;
-
-				byte mask = (byte) 0x7f;
-				System.out.println(mask & 0x7f);
-
-				buf[idx++] = h;
-				buf[idx++] = r;
-				buf[idx++] = g;
-				buf[idx++] = b;
-				buf[idx++] = mask;
-			}
-
-			for (int j = 0; j < 40; j++) {
-				byte h = (byte) 0x3f;
-				// byte r = r();
-				// byte g = r();
-				// byte b = r();
-				r = (byte) 0x80;
-				g = (byte) 0xff;
-				b = (byte) 0x80;
-
-				byte mask = (byte) 0x80;
-
-				buf[idx++] = h;
-				buf[idx++] = r;
-				buf[idx++] = g;
-				buf[idx++] = b;
-				buf[idx++] = mask;
+				buf[idx++] = id;
 			}
 
 			bos.write(buf, 0, idx);
-			// bos.write(0x00);
+
 			bos.flush();
 			int read = bis.read();
 			System.out.println(read);
 			try {
-				Thread.sleep(33);
+				Thread.sleep(100);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 		}
-
-		byte h = (byte) 0x7f;
-		byte r = (byte) 0x80;
-		byte g = (byte) 0x80;
-		byte b = (byte) 0x80;
-		byte mask = (byte) 0x70;
-		bos.write(h);
-		bos.write(r);
-		bos.write(g);
-		bos.write(b);
-		bos.write(mask);
-		bos.flush();
 
 		bos.write("end".getBytes());
 		bos.flush();
